89a23a84fa3d4223ba2d1e2aa7d43fa1556fd988
viionc <marek.d.stankiewicz@gmail.com>
2023-09-20 16:54:49 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
             <Navbar />
 
             <Routes>
-                <Route path="/home" element={<Home />} />
+                <Route path="" element={<Home />} />
                 <Route path="/store" element={<Store />} />
                 <Route path="/about" element={<About />} />
             </Routes>
